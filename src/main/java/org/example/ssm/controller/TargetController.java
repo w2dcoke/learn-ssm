60eaa064a19a092b9193f3e789a739dbe8bc4657
w2dcoke <w2dcoke@gmail.com>
2022-04-16 17:04:27 +0800

    update

---
+++ src/main/java/org/example/ssm/controller/TargetController.java
@@ -0,0 +1,20 @@
+package org.example.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author why
+ * @date 2022/4/16
+ */
+@Controller
+@RequestMapping("/target")
+public class TargetController {
+    @RequestMapping("/show")
+    public ModelAndView show(ModelAndView mv) {
+        mv.setViewName("index");
+        System.out.println(1/0);
+        return mv;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: why
+  Date: 2022/4/16
+  Time: 15:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello Target!</h1>
+</body>
+</html>
